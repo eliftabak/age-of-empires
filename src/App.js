8671af8c8b27c5d7e6fc936622fdaa23c7441fdb
Elif <elifchorghay@gmail.com>
2022-10-08 11:27:29 +0300

    Add header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.scss';
 import { Routes, Route,  } from "react-router-dom";
 import Home from './components/home';
 import UnitList from './components/unitList';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="units" element={<UnitList />} />
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './index.scss';
-import { Link } from "react-router-dom";
-import ageOfEmpiresBanner from '../../assets/img/age-of-empires-poster.jpeg';
+import ageOfEmpiresBanner from '../../assets/img/age-of-empires-banner.jpeg';
 
 function Home() {
   return (
     <div className='home'>
-      <div className="home__header">
-        <h1>Home Page</h1>
-        <div className='home__navLinks'>
-          <Link to="/">Home</Link>
-          <Link to="/units">Units</Link>
-        </div>
-      </div>
       <div className='home__bannerContainer'>
         <img src={ageOfEmpiresBanner} alt="age-of-empires-banner"/>
       </div>
