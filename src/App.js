388820b3ec3f40d5eac08f6b15b83fea6c7603b7
Elif <elifchorghay@gmail.com>
2022-10-12 23:43:48 +0300

    update sile structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { Routes, Route,  } from "react-router-dom";
 import Home from './components/home';
 import UnitList from './components/unitList';
+import UnitDetail from './components/unitDetail';
 import Header from './components/header';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="units" element={<UnitList />} />
+        <Route path="detail/:unitName" element={<UnitDetail />} />
       </Routes>
     </div>
   );
